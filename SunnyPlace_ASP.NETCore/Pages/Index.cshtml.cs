c65ba3f90b5d2ec643840e1cc8520e805fa7d240
Nayun Kim <ny.kim@tum.de>
2023-04-10 16:14:56 +0200

    create a map on the map page
    using leaflet library

--- SunnyPlace_ASP.NETCore/Pages/Index.cshtml.cs
+++ SunnyPlace_ASP.NETCore/Pages/Index.cshtml.cs
@@ -5,6 +5,7 @@ namespace SunnyPlace_ASP.NETCore.Pages;
 
 public class IndexModel : PageModel
 {
+    public string Message { get; private set; } = "PageModel in C#";
     private readonly ILogger<IndexModel> _logger;
 
     public IndexModel(ILogger<IndexModel> logger)
@@ -14,7 +15,7 @@ public class IndexModel : PageModel
 
     public void OnGet()
     {
-
+        Message += $"Sever time is {DateTime.Now}";
     }
 }
 
